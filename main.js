2e83a2f2686cec0503cdbf05ffb0949146d7ea61
Mahnoor kibrea <160492529+mahnoor-k29@users.noreply.github.com>
2024-05-07 14:24:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let personName = "mahnoor kibrea";
+console.log("hello", personName, " would you like to learn some python?");
+var person2Name = "mahnoor kibrea";
+console.log("hello", person2Name, "welcome to my new apartment ");
+const ab = "truth";
+console.log("its", ab, "that earth is round, sun is hot and moon is cold");
+let school = "students ";
+console.log("congratulation", school, "Our school got top position in sports  ");
